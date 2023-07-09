699e2d123d60629b628f42fa0467aa6c27a177c9
Tawitg <108408219+Tawitg@users.noreply.github.com>
2023-07-09 16:47:25 +0100

    OOP Update main.py
    Added Classes and methods to organise the code.

--- main.py
+++ main.py
@@ -1,118 +1,113 @@
-import  pyfiglet, subprocess, os, tkinter, re
+import pyfiglet
+import subprocess
+import os
+import tkinter
 import moviepy.editor as moviepy
 from tkinter import filedialog
 
-#Functions
 
-def search_for_video_file():
+class FileManipulation:
+    def __init__(self):
+        self.video_file_types = ("Video files", "*.mp4;*.avi;*.mkv;*.mov;*.m4a")
+        self.audio_file_types = ("Audio files", "*.mp3;*.wav;*.ogg")
 
-    tk = tkinter.Tk()
-    tk.withdraw()
+    def search_for_file(self, filetype):
+        tk = tkinter.Tk()
+        tk.withdraw()
 
-    filetypes = (
-        ("Video files", "*.mp4;*.avi;*.mkv;*.mov;*.m4a"),
-        ("All files", "*.*")
-    )
-    file_path = filedialog.askopenfilename(parent=tk, filetypes=filetypes)
-    if len(file_path) > 0:
-        print(f"Selected file : {file_path}")
-    return file_path
+        filetypes = ()
 
-def convert_video_to_mp3(input_file, bitrate, sample_rate):
-    input_file = os.path.abspath(input_file)
-    output_directory = os.path.join(os.path.dirname(__file__), 'Audios')
+        if filetype == "video":
+            filetypes = (self.video_file_types, ("All files", "*.*"))
+        elif filetype == "audio":
+            filetypes = (self.audio_file_types,("All files", "*.*"))
 
-    ffmpeg_cmd = [
-        
-        "ffmpeg",
-        "-loglevel", "quiet",
-        "-i", input_file,
-        "-vn",
-        "-acodec", "libmp3lame",
-        "-ab", str(f'{bitrate}k'),
-        "-ar", str(sample_rate),
-        "-y",
-        os.path.join(output_directory, f'{"".join(re.split(r".mp4|.avi|.mkv|.mov|.m4a","".join(os.path.basename(input_file))))}.mp3')
-    ]
-
-    try:
-        subprocess.run(ffmpeg_cmd, check=True)
-        print("Successfully converted!")
-
-    except subprocess.CalledProcessError as error:
-        print("Conversion failed.")
+        file_path = filedialog.askopenfilename(parent=tk, filetypes=filetypes)
+        if len(file_path) > 0:
+            print(f"Selected file: {file_path}")
+        return file_path
 
 
-def convert_to_mp4(input_file):
-    input_file = os.path.abspath(input_file)
+class FFmpegProcess(FileManipulation):
+    def __init__(self):
+        super().__init__()
+        self.audio_directory = os.path.join(os.path.dirname(__file__), 'Audios')
+        self.video_directory = os.path.join(os.path.dirname(__file__), 'Videos')
 
-    file = moviepy.VideoFileClip(input_file)
-    file.write_videofile(f'{os.path.basename(input_file)}.mp4')
-
-
-print(f"{pyfiglet.figlet_format('VideoTools')}", flush=True)
-
-#Commands 
-
-commands = [
-    'ytomp4 - Youtube to .mp4',
-    'yttomp3 - Youtube to .mp3',
-    'vidresc - Video resolution & aspect changer',
-    'vidspeedc - Video speed changer',
-    'vidtoaud - Video(.mp4|.avi|.mkv|.mov|.m4a) to aduio(.mp3)',
-    ]
-
-def comds():
-    for i in commands:
-        print(i)
-    
-
-def vidtoaud(bitrate, samplerate):
-    file = search_for_video_file()
-    convert_video_to_mp3(file, bitrate, samplerate)
-
-# Command line 
-
-def  menu():
-
-    choices={
-    'comds' : comds,
-    'vidtoaud' : vidtoaud,
-    }
-
-    print("Type comds for a list of commands")
-    
-    while True:
-
-        menu_choice=(input("\n>> ")).lower()
-        seperators=[","," ",]
-        command=re.split('|'.join(map(re.escape,seperators)), menu_choice)
-        command=[i for i in command if i != '']
-
-        bitrate=192
-        samplerate=44100
-
-        if command[0] in choices and len(command) > 1:
-            if command[0] == 'vidtoaud':
-                for i in command[1:]:
-                    i_name, i_value = i.split('=')
-                    if i_name == 'bitrate':
-                        bitrate = int(i_value)
-                    elif i_name == 'samplerate':
-                        samplerate = int(i_value)
-                choices[command[0]](bitrate,samplerate)
-                continue
-        else:
-            choices[command[0]](bitrate,samplerate)
-            continue
-        
-        choices[menu_choice]()
-
-menu()
+    def convert_video_to_audio(self, **kwargs):
+        file = self.search_for_file("video")
+        input_file = os.path.abspath(file)
+        output_directory = self.audio_directory
 
 
+        bitrate = int(kwargs.get('bitrate', 192))
+        sample_rate = int(kwargs.get('samplerate', 44100))
+
+        ffmpeg_cmd = [
+            "ffmpeg",   
+            "-loglevel", "quiet",
+            "-i", input_file,
+            "-vn",
+            "-acodec", "libmp3lame",
+            "-ab", f"{bitrate}k",
+            "-ar", str(sample_rate),
+            "-y",
+            os.path.join(output_directory, f"{os.path.basename(input_file).split('.')[0]}.mp3")
+        ]
+
+        try:
+            subprocess.run(ffmpeg_cmd, check=True)
+            print("Successfully converted!")
+        except subprocess.CalledProcessError as error:
+            print("Conversion failed.")
 
 
+class Commands(FFmpegProcess):
+    def __init__(self):
+        super().__init__()
+        self.commands = {
+            'comds': self.display_commands,
+            'vidtoaud': self.convert_video_to_audio
+        }
 
-        
-    
+    def display_commands(self):
+        commands = [
+            'ytomp4 - Youtube to .mp4',
+            'yttomp3 - Youtube to .mp3',
+            'vidresc - Video resolution & aspect changer',
+            'vidspeedc - Video speed changer',
+            'vidtoaud - Video(.mp4|.avi|.mkv|.mov|.m4a) to audio(.mp3)',
+        ]
+        for command in commands:
+            print(command)
+
+
+class Console(Commands):
+    def __init__(self):
+        super().__init__()
+        self.banner = pyfiglet.figlet_format('VideoTools')
+
+    def run(self):
+        print(self.banner, flush=True)
+        print("Type 'comds' for a list of commands")
+
+        while True:
+            menu_choice = input("\n>> ").lower()
+            command_parts = menu_choice.split()
+            command = command_parts[0]
+
+            if command in self.commands:
+                if len(command_parts) > 1:
+                    params = {}
+                    for arg in command_parts[1:]:
+                        key, value = arg.split('=')
+                        params[key] = value
+                    self.commands[command](**params)
+                else:
+                    self.commands[command]()
+            else:
+                print("Invalid command.")
+
+if __name__ == "__main__":
+    console = Console()
+    console.run()
