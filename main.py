10fdfab9e1160e9297790a801c885c1a03b7ce8a
Thomas Obungu <thomasobungus@gmail.com>
2023-11-27 19:31:10 +0000

    Update main.py
    Added comments to code.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from file_manipulation import FileManipulation
-from ffmpeg_process import FFmpegProcess
-from commands import Commands
-from console import Console
+from file_manipulation import FileManipulation #Imports classes from different files # Imports file management aspect of files
+from ffmpeg_process import FFmpegProcess #Imports the ffmpeg command shortcuts
+from commands import Commands #Imports commmands regarding ffmpeg for the console
+from console import Console #Imports the actual command line interface to enter the commands into
 
-def main():
+def main(): # Calling the classes
     FileManipulation()
     FFmpegProcess()
     Commands()
-    
+    # Setting varibles for the classes
     console = Console()
-    console.run()
+    console.run() # Running class through varible
 
 if __name__ == "__main__":
     main()
